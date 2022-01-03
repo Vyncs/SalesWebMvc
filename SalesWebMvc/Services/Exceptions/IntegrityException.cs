8040be2a8d14fc7c160634374b9114501fd959ac
Vinicius <vgg00@outlook.com.br>
2022-01-03 09:17:46 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/Exceptions/IntegrityException.cs
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace SalesWebMvc.Services.Exceptions
 {
-    public class IntegrityException : Application
+    public class IntegrityException : ApplicationException
     {
+        public IntegrityException(string message) : base(message)
+        {
 
+        }
     }
 }
